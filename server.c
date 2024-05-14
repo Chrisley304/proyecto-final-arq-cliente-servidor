083f2c0abe9d638e740a4abdbffa45c0c2989c53
Christian Leyva <53919610+Chrisley304@users.noreply.github.com>
2024-05-14 08:22:44 -0600

    Fixed server bug to work on MacOS

--- server.c
+++ server.c
@@ -1,14 +1,15 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <sys/types.h> // Include for accept()
+#include <unistd.h>
 
 #define PORT 8080
 
-int main() {
+int main()
+{
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
@@ -16,14 +17,14 @@ int main() {
     char buffer[1024] = { 0 };
     char* hello = "Hello from server";
 
-    //Creating socket file descriptor
+    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    //Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    // Forcefully attaching socket to the port 8080. Updated to work with MacOS
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -32,25 +33,25 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    //BIND THE SOCKET TO THE ADDRESS
+    // BIND THE SOCKET TO THE ADDRESS
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    //Listen for connections
+    // Listen for connections
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    //Accept incoming connection
+    // Accept incoming connection
     if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    //Read data from client
+    // Read data from client
     int valread;
     if ((valread = read(new_socket, buffer, 1024)) < 0) {
         perror("read failed");
@@ -58,14 +59,14 @@ int main() {
     }
     printf("command received: %s\n", buffer);
 
-    //Execute command and capture output
-    // ...
+    // Execute command and capture output
+    //  ...
 
-    //Send output back to client
+    // Send output back to client
     send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
 
-    //close the socket
+    // close the socket
     close(new_socket);
     close(server_fd);
     return 0;
