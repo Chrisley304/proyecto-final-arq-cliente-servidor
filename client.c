8f23338d055e3670cc86a00e4ecf5bf67e1098e9
Christian Leyva <53919610+Chrisley304@users.noreply.github.com>
2024-06-02 20:00:22 -0600

    Updated client and server to connect each other and exec the client commands on the server

--- client.c
+++ client.c
@@ -1,54 +1,82 @@
-#include <stdio.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define PORT 8080
+#define MAX_BUFFER_SIZE 1024
 
-int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char* command = "ls -la";
-    char buffer[1024] = { 0 };
+int main(int argc, char const* argv[])
+{
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n socket creation error \n");
-        return -1;
+    // Verificamos que el comando de terminal tenga los 3 argumentos necesarios
+    //./ejecutable IP Puerto
+    // Si no se detectan los 3 lo que haremos sera enviar un error
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <IP DEL SERVIDOR> <PUERTO>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    // Declaramos variable para el file descriptor del cliente
+    int file_descriptor_servidor = 0;
 
-    // Convert IPv4 and IPv6 address from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
+    // Declaramos la estructura para la informacion del servidor
+    struct sockaddr_in servidor_address;
+
+    // Creamos el socket del cliente con la funcion socket
+    // En caso de que exista un error lo mandamos
+    if ((file_descriptor_servidor = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Error al crear el socket");
+        exit(EXIT_FAILURE);
     }
 
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed\n");
-        return -1;
+    // Configuramos nuestra estrcutura para la info del servidor
+    servidor_address.sin_family = AF_INET;
+    servidor_address.sin_port = htons(atoi(argv[2]));
+
+    // Convertimos la dirección IP a binario para la conexion
+    // En caso de error mandamos mensaje
+    if (inet_pton(AF_INET, argv[1], &servidor_address.sin_addr) <= 0) {
+        perror("Dirección IP no válida");
+        exit(EXIT_FAILURE);
     }
 
-    if (send(sock, command, strlen(command), 0) < 0) {
-        printf("\nSending command failed\n");
-        return -1;
+    // Conectando con el servidor
+    // Utilizamos la funcion conect para establecer conexion con el socket del cliente
+    // En caso contrario mandamos error
+    if (connect(file_descriptor_servidor, (struct sockaddr*)&servidor_address, sizeof(servidor_address)) < 0) {
+        perror("Error al conectar");
+        exit(EXIT_FAILURE);
     }
-    printf("Command sent\n");
 
-    // Clear buffer
-    memset(buffer, 0, sizeof(buffer));
+    // Declaramos nuestra variable para almacenar el comando que venga de consola
+    char buffer[MAX_BUFFER_SIZE] = { 0 };
+    printf("Ingrese el comando a enviar al servidor: ");
+    // Con fgets vamos a leer una linea de texto desde el teclado
+    // Los parametros son: donde se almacena, tamaño maximo, indicamos que es de teclado
+    fgets(buffer, sizeof(buffer), stdin);
 
-    int valread;
-    if ((valread = read(sock, buffer, 1024)) < 0) {
-        printf("\nReading response failed\n");
-        return -1;
+    // Una vez leido el comando lo Enviamos al servidor
+    // En este caso usamos se usa el file descriptor del cliente, el comando, longitud de los datos a enviar
+    // El 0 es para indicar que no hay banderas adicionales
+    send(file_descriptor_servidor, buffer, strlen(buffer), 0);
+
+    // Recibimos la salida del servidor
+    printf("Respuesta del servidor:\n");
+
+    // Se ejecutara hasta que lleguemos a un break
+    while (1) {
+        // Recibiremos datos del servidor
+        // parametros fileDescriptor, lugar donde se alamcena, tamaño maximo de datos a recibir
+        ssize_t mensaje_recibido = recv(file_descriptor_servidor, buffer, sizeof(buffer), 0);
+        if (mensaje_recibido <= 0) {
+            break;
+        }
+        printf("%.*s", (int)mensaje_recibido, buffer);
     }
-    printf("%s\n", buffer);
 
-    // Close the socket
-    close(sock);
-
+    // Cerrar el socket y terminar el programa
+    close(file_descriptor_servidor);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,73 +1,127 @@
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h> // Include for accept()
 #include <unistd.h>
 
-#define PORT 8080
+#define DEFAULT_PORT 8080 // Definimos el puerto que usaremos
+#define MAX_BUFFER_SIZE 1024 // definimos el buffer maximo para poder almacenar datos
 
-int main()
+/**
+ * @brief Función que verifica si un string es un número.
+ * @param number String a verificar.
+ * @return int 1 si es un número, 0 si no lo es.
+ */
+int isNumber(char* number)
 {
-    int server_fd, new_socket;
+    int i = 0;
+
+    // checking for negative numbers
+    if (number[0] == '-')
+        i = 1;
+    for (; number[i] != 0; i++) {
+        // if (number[i] > '9' || number[i] < '0')
+        if (!isdigit(number[i]))
+            return 0;
+    }
+    return 1;
+}
+
+/**
+ * @brief Función principal del servidor SSH. Por defecto escucha en el puerto port 8080 en localhost (127.0.0.1).
+ * @param argc Número de argumentos pasados al programa.
+ * @param argv Argumentos pasados al programa. `argv[1]`(opcional) es el puerto en el que escuchará el servidor.
+ *
+ * @return int
+ */
+int main(int argc, char* argv[])
+{
+    int server_port = DEFAULT_PORT;
+    // Verificamos los argumentos pasados al programa, si hay alguno.
+    if (argc >= 2) {
+        // -h -> ayuda
+        if (strcmp(argv[1], "-h") == 0) {
+            fprintf(stderr, "Servidor SSH Demo -- Proyecto Final Arquitectura Cliente-Servidor\n\nComandos:\n- ./server -h: Muestra mensaje de ayuda.\n./server: Inicializa el servidor con el puerto default 8080 en localhost (127.0.0.1).\n./server <NUMERO_PUERTO>: Inicializa el servidor en localhost (127.0.0.1) utilizando el puerto ingresado.\n\n");
+            exit(EXIT_SUCCESS);
+        } else {
+            // [int] -> puerto
+            if (isNumber(argv[1])) {
+                server_port = atoi(argv[1]);
+            } else {
+                fprintf(stderr, "Error: El puerto '%s' es incorrecto. El puerto debe ser un número entero.\n", argv[1]);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    int server_fd, new_socket; // File Descriptor og servidor
     struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
+    int addrlen = sizeof(address); // tamaño de direccion
 
-    // Creating socket file descriptor
+    // Creación del socket para el servidor.
+    // Sí la respuesta es diferente de 0, un error es enviado al conectar.
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+        perror("Error al crear el socket");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080. Updated to work with MacOS
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    address.sin_family = AF_INET; // La direccion del servidor sera de tipo AF_INET
+    address.sin_addr.s_addr = INADDR_ANY; // Direccion ip que utilizaremos (localhost -> 127.0.0.1)
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(server_port); // Puerto que el servidor escuchara
 
-    // BIND THE SOCKET TO THE ADDRESS
+    // Enlazando nuestro socket a la dirección y el puerto indicado
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+        perror("Error en el enlace");
         exit(EXIT_FAILURE);
     }
 
-    // Listen for connections
+    // Escuchar conexiones
+    // Utilizamos la funcion listen para escuchar las conexiones
+    // listen recibe el file descriptor del servidor, el numero de conexiones
+    // Verificamos si la respuesta regresa -1 para validar si hay errores
+    // En este caso podemos manejar hasta 3 conexiones entrantes
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
+        perror("Error al escuchar");
         exit(EXIT_FAILURE);
     }
 
-    // Accept incoming connection
+    printf("Servidor en IP 127.0.0.1 (localhost) escuchando en el puerto %d\n", server_port);
+
+    // Aceptamos la conexión entrante
+    // Una vez aceptada la conexion creamos un nuevo socket para la comunicacion con el cliente
+    // En caso de que exista un error mandamos el mensaje
     if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
+        perror("Error al aceptar la conexión");
         exit(EXIT_FAILURE);
     }
 
-    // Read data from client
-    int valread;
-    if ((valread = read(new_socket, buffer, 1024)) < 0) {
-        perror("read failed");
+    char buffer[MAX_BUFFER_SIZE] = { 0 };
+
+    // Leemos el comando enviado por el cliente y lo almacenamos en el buffer
+    // Posterior imprimimos el comando recibido
+    read(new_socket, buffer, sizeof(buffer));
+    printf("Comando recibido: %s\n", buffer);
+
+    // Ejecutamos el comando y obtenemos salida
+    // con el comando popen se abre un proceso que ejecuta un comando como si estuvieramos en terminal
+    // El primer argumento de popen es el comando en este caso buffer
+    // El segundo argumento es como abrimos el archivo en este caso para lectura
+    // En caso de error mandamos el error
+    FILE* fp = popen(buffer, "r");
+    if (fp == NULL) {
+        perror("Error al ejecutar el comando");
         exit(EXIT_FAILURE);
     }
-    printf("command received: %s\n", buffer);
 
-    // Execute command and capture output
-    //  ...
+    // Leemos la salida del comando y la enviamos al cliente
+    // mientras el buffer tenga caracteres seguimos enviando la informacion al cliente
+    while (fgets(buffer, sizeof(buffer) - 1, fp) != NULL) {
+        send(new_socket, buffer, strlen(buffer), 0);
+    }
 
-    // Send output back to client
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-
-    // close the socket
+    // Cerrar el socket y terminar el programa
     close(new_socket);
-    close(server_fd);
     return 0;
 }
